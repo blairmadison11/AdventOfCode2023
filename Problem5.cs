197414d2da427252f36a5fb2fe0c5c3fa3ea988c
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 02:48:13 -0500

    Update Problem5.cs

--- Problem5.cs
+++ Problem5.cs
@@ -1,38 +1,29 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
-namespace Program
+List<string> lines = new List<string>();
+lines.Add(new string('.', 142));
+using (StreamReader reader = new StreamReader("D:\\input.txt"))
 {
-    public class Program
+    while (!reader.EndOfStream)
     {
-        public static void Main(string[] args)
-        {
-            List<string> lines = new List<string>();
-            lines.Add(new string('.', 142));
-            using (StreamReader reader = new StreamReader("D:\\input.txt"))
-            {
-                while (!reader.EndOfStream)
-                {
-                    lines.Add(string.Format(".{0}.", reader.ReadLine()));
-                }
-            }
-            lines.Add(new string('.', 142));
+        lines.Add(string.Format(".{0}.", reader.ReadLine()));
+    }
+}
+lines.Add(new string('.', 142));
 
-            int sum = 0;
-            for (int i = 1; i < lines.Count - 1; ++i)
-            {
-                MatchCollection matches = Regex.Matches(lines[i], @"\d+");
-                foreach (Match match in matches)
-                {
-                    int startX = match.Index - 1, length = match.Length + 2, endX = match.Index + match.Length;
-                    if (Regex.IsMatch(lines[i - 1].Substring(startX, length), @"[^.\d]") ||
-                        Regex.IsMatch(lines[i + 1].Substring(startX, length), @"[^.\d]") ||
-                            lines[i][startX] != '.' || lines[i][endX] != '.')
-                    {
-                        sum += int.Parse(match.Value);
-                    }
-                }
-            }
-            Console.WriteLine(sum);
+int sum = 0;
+for (int i = 1; i < lines.Count - 1; ++i)
+{
+    MatchCollection matches = Regex.Matches(lines[i], @"\d+");
+    foreach (Match match in matches)
+    {
+        int startX = match.Index - 1, length = match.Length + 2, endX = match.Index + match.Length;
+        if (Regex.IsMatch(lines[i - 1].Substring(startX, length), @"[^.\d]") ||
+            Regex.IsMatch(lines[i + 1].Substring(startX, length), @"[^.\d]") ||
+                lines[i][startX] != '.' || lines[i][endX] != '.')
+        {
+            sum += int.Parse(match.Value);
         }
     }
 }
+Console.WriteLine(sum);
