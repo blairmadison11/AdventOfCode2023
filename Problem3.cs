6bf71cfab13f61be0912096991d3bd5ecca6dd1d
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 18:03:51 -0500

    Update Problem3.cs

--- Problem3.cs
+++ Problem3.cs
@@ -1,43 +1,21 @@
 using System.Text.RegularExpressions;
 
-int idSum = 0;
-Dictionary<string, int> MAX_VALUES = new Dictionary<string, int>()
+var sum = 0;
+Dictionary<string, int> maxVals = new Dictionary<string, int>() { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
+foreach (string line in File.ReadAllLines("D:\\input.txt"))
 {
-    { "red", 12 },
-    { "green", 13 },
-    { "blue", 14 }
-};
-
-using (StreamReader reader = new StreamReader("D:\\input.txt"))
-{
-    while (!reader.EndOfStream)
+    var match = Regex.Match(line, @"Game (\d*): (?:((\d*) (red|green|blue),?\s?)+;?\s?)+");
+    var counts = Array.ConvertAll(match.Groups[3].Captures.ToArray(), s => int.Parse(s.Value));
+    var colors = Array.ConvertAll(match.Groups[4].Captures.ToArray(), s => s.Value);
+    var possibleFlag = true;
+    for (int i = 0; i < counts.Length; ++i)
     {
-        bool possibleFlag = true;
-        string line = reader.ReadLine();
-        Match match1 = Regex.Match(line, @"Game (\d*): (?:([^;]*);?\s?)+");
-        foreach (Capture capture in match1.Groups[2].Captures)
+        if (counts[i] > maxVals[colors[i]])
         {
-            Match match2 = Regex.Match(capture.Value, @"((\d*) (red|green|blue),?\s?)+");
-            CaptureCollection counts = match2.Groups[2].Captures;
-            CaptureCollection colors = match2.Groups[3].Captures;
-            for (int i = 0; i < colors.Count; ++i)
-            {
-                int count = int.Parse(counts[i].Value);
-                if (count > MAX_VALUES[colors[i].Value])
-                {
-                    possibleFlag = false;
-                    break;
-                }
-            }
-            if (!possibleFlag)
-            {
-                break;
-            }
-        }
-        if (possibleFlag)
-        {
-            idSum += int.Parse(match1.Groups[1].Value);
+            possibleFlag = false;
+            break;
         }
     }
+    if (possibleFlag) sum += int.Parse(match.Groups[1].Value);
 }
-Console.WriteLine(idSum);
+Console.WriteLine(sum);
