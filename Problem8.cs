8a3178a319b5be05988f8509322ec9a6bbd516cf
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 15:53:55 -0500

    Update Problem8.cs

--- Problem8.cs
+++ Problem8.cs
@@ -1,14 +1,14 @@
 using System.Text.RegularExpressions;
 
-string[] lines = File.ReadAllLines("D:\\input.txt");
-int[] cards = Enumerable.Repeat(1, lines.Length).ToArray();
-for (int i = 0; i < lines.Length; i++)
+var lines = File.ReadAllLines("D:\\input.txt");
+var cards = Enumerable.Repeat(1, lines.Length).ToArray();
+for (var i = 0; i < lines.Length; i++)
 {
-    Match parse = Regex.Match(lines[i], @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
-    HashSet<int> winningNums = new HashSet<int>(Array.ConvertAll(parse.Groups[2].Captures.ToArray(), s => int.Parse(s.Value)));
-    HashSet<int> scratchNums = new HashSet<int>(Array.ConvertAll(parse.Groups[4].Captures.ToArray(), s => int.Parse(s.Value)));
-    int points = scratchNums.Intersect(winningNums).Count();
-    for (int j = i + 1; j < i + points + 1; ++j)
+    var parse = Regex.Match(lines[i], @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
+    var winningNums = new HashSet<int>(Array.ConvertAll(parse.Groups[2].Captures.ToArray(), s => int.Parse(s.Value)));
+    var scratchNums = new HashSet<int>(Array.ConvertAll(parse.Groups[4].Captures.ToArray(), s => int.Parse(s.Value)));
+    var points = scratchNums.Intersect(winningNums).Count();
+    for (var j = i + 1; j < i + points + 1; ++j)
     {
         cards[j] += cards[i];
     }
