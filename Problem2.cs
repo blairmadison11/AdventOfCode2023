3972498efd53ee471f0431fc16eba9cee2ea896d
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 15:56:17 -0500

    Update Problem2.cs

--- Problem2.cs
+++ Problem2.cs
@@ -4,7 +4,7 @@ var digits = new Dictionary<string, int>() { {"one", 1 }, {"two", 2 }, {"three",
 var pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
 var GetNum = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : digits[m];
 var total = 0;
-foreach (string line in File.ReadLines("D:\\input.txt"))
+foreach (var line in File.ReadLines("D:\\input.txt"))
 {
     total += (GetNum(Regex.Match(line, pattern).Value) * 10) + GetNum(Regex.Match(line, pattern, RegexOptions.RightToLeft).Value);
 }
