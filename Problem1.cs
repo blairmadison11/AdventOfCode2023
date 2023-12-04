4ba8435daa812a9f841465da12f6adc40c798dc1
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 15:55:15 -0500

    Update Problem1.cs

--- Problem1.cs
+++ Problem1.cs
@@ -1,7 +1,7 @@
 using System.Text.RegularExpressions;
 
-int total = 0;
-foreach (string line in File.ReadAllLines("D:\\input.txt"))
+var total = 0;
+foreach (var line in File.ReadAllLines("D:\\input.txt"))
 {
     total += (int.Parse(Regex.Match(line, "\\d").Value) * 10) + int.Parse(Regex.Match(line, "\\d", RegexOptions.RightToLeft).Value);
 }
