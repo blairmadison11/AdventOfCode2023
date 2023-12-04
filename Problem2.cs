789eca83c6a2f5dff7574e3332c397d9f90cc69e
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 15:47:38 -0500

    Update Problem2.cs

--- Problem2.cs
+++ Problem2.cs
@@ -1,54 +1,11 @@
 using System.Text.RegularExpressions;
 
-namespace Program
+var digits = new Dictionary<string, int>() { {"one", 1 }, {"two", 2 }, {"three", 3 }, {"four", 4 }, {"five", 5 }, {"six", 6 }, {"seven", 7 }, {"eight", 8 }, {"nine", 9 } };
+var pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
+var GetNum = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : digits[m];
+var total = 0;
+foreach (string line in File.ReadLines("D:\\input.txt"))
 {
-    public class Program
-    {
-        static string AOC_PATTERN = @"\d|one|two|three|four|five|six|seven|eight|nine";
-        static Dictionary<string, int> DIGIT_WORDS = new Dictionary<string, int>()
-        {
-            {"one", 1 },
-            {"two", 2 },
-            {"three", 3 },
-            {"four", 4 },
-            {"five", 5 },
-            {"six", 6 },
-            {"seven", 7 },
-            {"eight", 8 },
-            {"nine", 9 }
-        };
-
-        public static void Main(string[] args)
-        {
-            int total = 0;
-            using (StreamReader reader = new StreamReader("D:\\input.txt"))
-            {
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine();
-                    Match leftMatch = Regex.Match(line, AOC_PATTERN);
-                    Match rightMatch = Regex.Match(line, AOC_PATTERN, RegexOptions.RightToLeft);
-                    total += (GetNumberFromMatch(leftMatch) * 10) + GetNumberFromMatch(rightMatch);
-                }
-            }
-            Console.WriteLine(total);
-        }
-
-        public static int GetNumberFromMatch(Match match)
-        {
-            if (Char.IsDigit(match.Value[0]))
-            {
-                return match.Value[0] - '0';
-            }
-            else
-            {
-                return DIGIT_WORDS[match.Value];
-            }
-        }
-    }
+    total += (GetNum(Regex.Match(line, pattern).Value) * 10) + GetNum(Regex.Match(line, pattern, RegexOptions.RightToLeft).Value);
 }
-
-
-
-
-
+Console.WriteLine(total);
