c95b678a57aef5edd11c95df2ae0cc3316cc1d62
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 18:12:16 -0500

    Update Problem2.cs

--- Problem2.cs
+++ Problem2.cs
@@ -1,6 +1,6 @@
 using System.Text.RegularExpressions;
 
-var digits = new Dictionary<string, int>() { {"one", 1 }, {"two", 2 }, {"three", 3 }, {"four", 4 }, {"five", 5 }, {"six", 6 }, {"seven", 7 }, {"eight", 8 }, {"nine", 9 } };
+var digits = new Dictionary<string, int>() {{"one",1},{"two",2},{"three",3},{"four",4},{"five",5},{"six",6},{"seven",7},{"eight",8},{"nine",9}};
 var pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
 var GetNum = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : digits[m];
 var total = 0;
