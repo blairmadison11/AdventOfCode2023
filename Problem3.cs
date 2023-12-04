5125c433401a6211f9f20fdf79022fc6605e5318
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 18:05:14 -0500

    Update Problem3.cs

--- Problem3.cs
+++ Problem3.cs
@@ -1,14 +1,14 @@
 using System.Text.RegularExpressions;
 
 var sum = 0;
-Dictionary<string, int> maxVals = new Dictionary<string, int>() { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
-foreach (string line in File.ReadAllLines("D:\\input.txt"))
+var maxVals = new Dictionary<string, int>() { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
+foreach (var line in File.ReadAllLines("D:\\input.txt"))
 {
     var match = Regex.Match(line, @"Game (\d*): (?:((\d*) (red|green|blue),?\s?)+;?\s?)+");
     var counts = Array.ConvertAll(match.Groups[3].Captures.ToArray(), s => int.Parse(s.Value));
     var colors = Array.ConvertAll(match.Groups[4].Captures.ToArray(), s => s.Value);
     var possibleFlag = true;
-    for (int i = 0; i < counts.Length; ++i)
+    for (var i = 0; i < counts.Length; ++i)
     {
         if (counts[i] > maxVals[colors[i]])
         {
