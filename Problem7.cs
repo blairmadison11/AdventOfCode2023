9531e16563f021f1aacfe0bb5ff782ba68da8a37
Blair Durkee <blairmadison11@gmail.com>
2023-12-04 15:52:15 -0500

    Update Problem7.cs

--- Problem7.cs
+++ Problem7.cs
@@ -1,11 +1,11 @@
 using System.Text.RegularExpressions;
 
-int pointSum = 0;
+var pointSum = 0;
 foreach (string line in File.ReadAllLines("D:\\input.txt"))
 {
-    Match parse = Regex.Match(line, @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
-    HashSet<int> winningNums = new HashSet<int>(Array.ConvertAll(parse.Groups[2].Captures.ToArray(), s => int.Parse(s.Value)));
-    HashSet<int> scratchNums = new HashSet<int>(Array.ConvertAll(parse.Groups[4].Captures.ToArray(), s => int.Parse(s.Value)));
+    var parse = Regex.Match(line, @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
+    var winningNums = new HashSet<int>(Array.ConvertAll(parse.Groups[2].Captures.ToArray(), s => int.Parse(s.Value)));
+    var scratchNums = new HashSet<int>(Array.ConvertAll(parse.Groups[4].Captures.ToArray(), s => int.Parse(s.Value)));
     pointSum += (int)Math.Pow(2, scratchNums.Intersect(winningNums).Count() - 1);
 }
 Console.WriteLine(pointSum);
