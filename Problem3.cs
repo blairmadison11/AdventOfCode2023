f43c867dae379da01a7967fe1605d32928dd174c
Blair Durkee <blairmadison11@gmail.com>
2023-12-05 00:07:32 -0500

    Update Problem3.cs

--- Problem3.cs
+++ Problem3.cs
@@ -8,12 +8,11 @@ foreach (var line in File.ReadAllLines("D:\\input.txt"))
     var counts = Array.ConvertAll(match.Groups[3].Captures.ToArray(), s => int.Parse(s.Value));
     var colors = Array.ConvertAll(match.Groups[4].Captures.ToArray(), s => s.Value);
     var possibleFlag = true;
-    for (var i = 0; i < counts.Length; ++i)
+    for (var i = 0; i < counts.Length && possibleFlag; ++i)
     {
         if (counts[i] > maxVals[colors[i]])
         {
             possibleFlag = false;
-            break;
         }
     }
     if (possibleFlag) sum += int.Parse(match.Groups[1].Value);
