09402b3bc43afd59c7b69ac97a12f64ff88dd64d
Blair Durkee <blairmadison11@gmail.com>
2023-12-06 23:50:07 -0500

    Update Day02_Part2.cs

--- Day02_Part2.cs
+++ Day02_Part2.cs
@@ -5,8 +5,8 @@ foreach (var line in File.ReadAllLines("D:\\input.txt"))
 {
     var cubeCounts = new Dictionary<string, int>();
     var match = Regex.Match(line, @"Game (\d*): (?:((\d*) (red|green|blue),?\s?)+;?\s?)+");
-    var counts = Array.ConvertAll(match.Groups[3].Captures.ToArray(), s => int.Parse(s.Value));
-    var colors = Array.ConvertAll(match.Groups[4].Captures.ToArray(), s => s.Value);
+    var counts = match.Groups[3].Captures.Select(s => int.Parse(s.Value));
+    var colors = match.Groups[4].Captures.Select(s => s.Value);
     for (var i = 0; i < counts.Length; ++i)
     {
         if (!cubeCounts.ContainsKey(colors[i]) || cubeCounts[colors[i]] < counts[i])
