30b1fd72f29395eeb4272c1bf525d02e5e65ecef
Blair Durkee <blairmadison11@gmail.com>
2023-12-06 23:32:25 -0500

    Update Day06_Part2.cs

--- Day06_Part2.cs
+++ Day06_Part2.cs
@@ -1,4 +1,4 @@
 using System.Text.RegularExpressions;
 
-var vals = File.ReadAllLines("D:\\input2.txt").Select(l => long.Parse(String.Join("", Array.ConvertAll(Regex.Match(l, @"(?:(\d+)\s*)+").Groups[1].Captures.ToArray(), c => c.Value)))).ToArray();
+var vals = File.ReadAllLines("D:\\input2.txt").Select(l => long.Parse(String.Join("", Regex.Match(l, @"(?:(\d+)\s*)+").Groups[1].Captures.Select(c => c.Value)))).ToArray();
 Console.WriteLine(Math.Floor((-vals[0] + Math.Sqrt(Math.Pow(vals[0], 2) - (4 * vals[1]))) / 2 - Math.Floor((-vals[0] - Math.Sqrt(Math.Pow(vals[0], 2) - (4 * vals[1]))) / 2)));
