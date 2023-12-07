66e7218219991f7666da2ed4c12a18d0af6b903b
Blair Durkee <blairmadison11@gmail.com>
2023-12-06 23:51:08 -0500

    Update Day04_Part1.cs

--- Day04_Part1.cs
+++ Day04_Part1.cs
@@ -4,8 +4,8 @@ var pointSum = 0;
 foreach (string line in File.ReadAllLines("D:\\input.txt"))
 {
     var parse = Regex.Match(line, @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
-    var winningNums = new HashSet<int>(Array.ConvertAll(parse.Groups[2].Captures.ToArray(), s => int.Parse(s.Value)));
-    var scratchNums = new HashSet<int>(Array.ConvertAll(parse.Groups[4].Captures.ToArray(), s => int.Parse(s.Value)));
+    var winningNums = new HashSet<int>(parse.Groups[2].Captures.Select(s => int.Parse(s.Value)));
+    var scratchNums = new HashSet<int>(parse.Groups[4].Captures.Select(s => int.Parse(s.Value)));
     pointSum += (int)Math.Pow(2, scratchNums.Intersect(winningNums).Count() - 1);
 }
 Console.WriteLine(pointSum);
