d6096cd7f409fc9b00b75e32ca4151ca5155b222
Blair Durkee <blairmadison11@gmail.com>
2023-12-06 23:25:47 -0500

    Update Day06_Part2.cs

--- Day06_Part2.cs
+++ Day06_Part2.cs
@@ -1,6 +1,4 @@
 using System.Text.RegularExpressions;
 
-var lines = File.ReadAllLines("D:\\input.txt");
-var time = long.Parse(Array.ConvertAll(Regex.Match(lines[0], @"Time:\s+(?:(\d+)\s*)+").Groups[1].Captures.ToArray(), c => c.Value).Aggregate((x, y) => x + y));
-var dist = long.Parse(Array.ConvertAll(Regex.Match(lines[1], @"Distance:\s+(?:(\d+)\s*)+").Groups[1].Captures.ToArray(), c => c.Value).Aggregate((x, y) => x + y));
-Console.WriteLine((int)((-time + Math.Sqrt(Math.Pow(time, 2) - (4 * dist))) / 2) - (int)((-time - Math.Sqrt(Math.Pow(time, 2) - (4 * dist))) / 2));
+var vals = File.ReadAllLines("D:\\input.txt").Select(l => long.Parse(String.Join("", Array.ConvertAll(Regex.Match(l, @"(?:(\d+)\s*)+").Groups[1].Captures.ToArray(), c => c.Value)))).ToArray();
+Console.WriteLine((int)((-vals[0] + Math.Sqrt(Math.Pow(vals[0], 2) - (4 * vals[1]))) / 2) - (int)((-vals[0] - Math.Sqrt(Math.Pow(vals[0], 2) - (4 * vals[1]))) / 2));
