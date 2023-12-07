62bec5ef1f3c119a079e825ab56943c0f9f5d74d
Blair Durkee <blairmadison11@gmail.com>
2023-12-07 14:58:24 -0500

    Update Day01_Part2.cs

--- Day01_Part2.cs
+++ Day01_Part2.cs
@@ -1,6 +1,6 @@
 using System.Text.RegularExpressions;
 
-var digits = new Dictionary<string, int>() {{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
-var GetNum = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : digits[m];
-var pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
-Console.WriteLine(File.ReadLines("D:\\input.txt").Select(l => GetNum(Regex.Match(l, pattern).Value) * 10 + GetNum(Regex.Match(l, pattern, RegexOptions.RightToLeft).Value)).Aggregate((x,y) => x + y));
+var lookup = new Dictionary<string, int>() { { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, { "five", 5 }, { "six", 6 }, { "seven", 7 }, { "eight", 8 }, { "nine", 9 } };
+var eval = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : lookup[m];
+var p = @"\d|one|two|three|four|five|six|seven|eight|nine";
+Console.WriteLine(File.ReadLines("D:\\input.txt").Select(l => eval(Regex.Match(l, p).Value) * 10 + eval(Regex.Match(l, p, RegexOptions.RightToLeft).Value)).Aggregate((x, y) => x + y));
