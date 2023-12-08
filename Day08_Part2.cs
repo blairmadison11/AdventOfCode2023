abfa2c641cc737a1f493c417319eed7577787ccf
Blair Durkee <blairmadison11@gmail.com>
2023-12-08 02:49:56 -0500

    Create Day08_Part2.cs

---
+++ Day08_Part2.cs
@@ -0,0 +1,37 @@
+using System.Text.RegularExpressions;
+
+var gcf = (ulong a, ulong b) =>
+{
+    while (a != 0 && b != 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a | b;
+};
+var map = new Dictionary<string, Tuple<string,string>>();
+var lines = File.ReadAllLines("D:\\input.txt");
+var directions = lines[0].ToArray();
+for (int i = 2; i < lines.Length; ++i)
+{
+    var matches = Regex.Matches(lines[i], @"[A-Z]+");
+    map.Add(matches[0].Value, new Tuple<string, string>(matches[1].Value, matches[2].Value));
+}
+var curLocs = map.Keys.Where(k => k.EndsWith("A")).ToHashSet();
+var stepCounts = new List<ulong>();
+var dirIndex = 0;
+ulong curSteps = 1;
+while (curLocs.Count > 0)
+{
+    curLocs = curLocs.Select(c => directions[dirIndex] == 'L' ? map[c].Item1 : map[c].Item2).ToHashSet();
+    foreach (var loc in curLocs.Where(c => c.EndsWith("Z")))
+    {
+        stepCounts.Add(curSteps);
+        curLocs.Remove(loc);
+    }
+    dirIndex = (dirIndex + 1) % directions.Length;
+    ++curSteps;
+}
+Console.WriteLine(stepCounts.Aggregate((a, b) => (a / gcf(a, b)) * b));
