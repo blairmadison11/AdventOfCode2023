8907fedeeb519823c982d863608ec9b3d2c1a36c
Blair Durkee <blairmadison11@gmail.com>
2023-12-09 01:03:09 -0500

    Update Day01_Part2.cs

--- Day01_Part2.cs
+++ Day01_Part2.cs
@@ -3,4 +3,4 @@ using System.Text.RegularExpressions;
 var lookup = new Dictionary<string, int>() { { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, { "five", 5 }, { "six", 6 }, { "seven", 7 }, { "eight", 8 }, { "nine", 9 } };
 var eval = (string m) => Char.IsDigit(m[0]) ? m[0] - '0' : lookup[m];
 var p = @"\d|one|two|three|four|five|six|seven|eight|nine";
-Console.WriteLine(File.ReadLines("D:\\input.txt").Select(l => eval(Regex.Match(l, p).Value) * 10 + eval(Regex.Match(l, p, RegexOptions.RightToLeft).Value)).Aggregate((x, y) => x + y));
+Console.WriteLine(File.ReadLines("D:\\input.txt").Select(l => eval(Regex.Match(l, p).Value) * 10 + eval(Regex.Match(l, p, RegexOptions.RightToLeft).Value)).Sum());
