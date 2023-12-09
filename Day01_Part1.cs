3ed70c74a5dc4e777eefb92c68ed32a0585e210a
Blair Durkee <blairmadison11@gmail.com>
2023-12-09 01:01:34 -0500

    Update Day01_Part1.cs

--- Day01_Part1.cs
+++ Day01_Part1.cs
@@ -1,3 +1,3 @@
 using System.Text.RegularExpressions;
 
-Console.WriteLine(File.ReadAllLines("D:\\input.txt").Select(l => (int.Parse(Regex.Match(l, "\\d").Value) * 10) + int.Parse(Regex.Match(l, "\\d", RegexOptions.RightToLeft).Value)).Aggregate((x, y) => x + y));
+Console.WriteLine(File.ReadAllLines("D:\\input.txt").Select(l => (int.Parse(Regex.Match(l, "\\d").Value) * 10) + int.Parse(Regex.Match(l, "\\d", RegexOptions.RightToLeft).Value)).Sum());
