9d339c1bd0fb87d3b5747b3aa31fb477d538b0c0
Blair Durkee <blairmadison11@gmail.com>
2023-12-15 01:15:49 -0500

    Update Day04_Part1.cs

--- Day04_Part1.cs
+++ Day04_Part1.cs
@@ -1,6 +1,6 @@
 using System.Text.RegularExpressions;
 
-Console.WriteLine(File.ReadAllLines("D:\\input.txt").Select(l => {
+Console.WriteLine(File.ReadAllLines(@"D:\input.txt").Select(l => {
     var m = Regex.Match(l, @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
     return (int)Math.Pow(2, m.Groups[4].Captures.Select(s => s.Value).Intersect(m.Groups[2].Captures.Select(s => s.Value)).Count() - 1);
 }).Sum());
