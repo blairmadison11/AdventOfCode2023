21d61aaf75e5f9a3d18d1bc5c4739512b4ddc7e1
Blair Durkee <blairmadison11@gmail.com>
2023-12-15 01:14:18 -0500

    Update Day04_Part2.cs

--- Day04_Part2.cs
+++ Day04_Part2.cs
@@ -5,7 +5,7 @@ var cards = Enumerable.Repeat(1, lines.Length).ToArray();
 for (var i = 0; i < lines.Length; i++)
 {
     var m = Regex.Match(lines[i], @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
-    var p = m.Groups[4].Captures.Select(s => int.Parse(s.Value)).Intersect(m.Groups[2].Captures.Select(s => int.Parse(s.Value))).Count();
+    var p = m.Groups[4].Captures.Select(s => s.Value).Intersect(m.Groups[2].Captures.Select(s => s.Value)).Count();
     for (var j = i + 1; j < i + p + 1; ++j)
     {
         cards[j] += cards[i];
