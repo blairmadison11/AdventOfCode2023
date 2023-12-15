b9af012e9ba00aa08d77f0f900e3a2d7cd4787d5
Blair Durkee <blairmadison11@gmail.com>
2023-12-15 01:13:05 -0500

    Update Day04_Part2.cs

--- Day04_Part2.cs
+++ Day04_Part2.cs
@@ -4,11 +4,9 @@ var lines = File.ReadAllLines("D:\\input.txt");
 var cards = Enumerable.Repeat(1, lines.Length).ToArray();
 for (var i = 0; i < lines.Length; i++)
 {
-    var parse = Regex.Match(lines[i], @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
-    var winningNums = new HashSet<int>(parse.Groups[2].Captures.Select(s => int.Parse(s.Value)));
-    var scratchNums = new HashSet<int>(parse.Groups[4].Captures.Select(s => int.Parse(s.Value)));
-    var points = scratchNums.Intersect(winningNums).Count();
-    for (var j = i + 1; j < i + points + 1; ++j)
+    var m = Regex.Match(lines[i], @"Card\s+\d+:\s+((\d+)\s*)+\|\s+((\d+)\s*)+");
+    var p = m.Groups[4].Captures.Select(s => int.Parse(s.Value)).Intersect(m.Groups[2].Captures.Select(s => int.Parse(s.Value))).Count();
+    for (var j = i + 1; j < i + p + 1; ++j)
     {
         cards[j] += cards[i];
     }
