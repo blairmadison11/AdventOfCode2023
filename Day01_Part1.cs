12419a793bdff148ae45e080b1c33f7f70f47b15
Blair Durkee <blairmadison11@gmail.com>
2023-12-15 01:16:43 -0500

    Update Day01_Part1.cs

--- Day01_Part1.cs
+++ Day01_Part1.cs
@@ -1,3 +1,3 @@
 using System.Text.RegularExpressions;
 
-Console.WriteLine(File.ReadAllLines("D:\\input.txt").Select(l => int.Parse(Regex.Match(l, @"\d").Value + Regex.Match(l, @"\d", RegexOptions.RightToLeft).Value)).Sum());
+Console.WriteLine(File.ReadAllLines("input.txt").Select(l => int.Parse(Regex.Match(l, @"\d").Value + Regex.Match(l, @"\d", RegexOptions.RightToLeft).Value)).Sum());
