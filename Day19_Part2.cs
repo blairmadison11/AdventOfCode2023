7059e78512402aa51c0bb79bca90b21c2b0c1da4
Blair Durkee <blairmadison11@gmail.com>
2023-12-19 11:49:12 -0500

    Update Day19_Part2.cs

--- Day19_Part2.cs
+++ Day19_Part2.cs
@@ -1,8 +1,8 @@
 using System.Text.RegularExpressions;
 
 var lines = File.ReadAllLines("input.txt");
-var workflows = new Dictionary<string, List<Func<Dictionary<string, (ulong, ulong)>, Dictionary<string, (ulong, ulong)>>>>();
-var wfcomplements = new Dictionary<string, List<Func<Dictionary<string, (ulong, ulong)>, Dictionary<string, (ulong, ulong)>>>>();
+var workflows = new Dictionary<string, List<Func<Dictionary<string, (int, int)>, Dictionary<string, (int, int)>>>>();
+var wfcomplements = new Dictionary<string, List<Func<Dictionary<string, (int, int)>, Dictionary<string, (int, int)>>>>();
 var destinations = new Dictionary<string, List<string>>();
 var fallbacks = new Dictionary<string, string>();
 string line;
@@ -12,24 +12,24 @@ while ((line = lines[li++]) != "")
     var m = Regex.Match(line, @"(\w+){(?:(\w+)([\<\>])(\d+):(\w+),?)+(\w+)}");
     var src = m.Groups[1].Value;
     var fbk = m.Groups[6].Value;
-    var wfList = new List<Func<Dictionary<string, (ulong, ulong)>, Dictionary<string, (ulong, ulong)>>>();
-    var wfcList = new List<Func<Dictionary<string, (ulong, ulong)>, Dictionary<string, (ulong, ulong)>>>();
+    var wfList = new List<Func<Dictionary<string, (int, int)>, Dictionary<string, (int, int)>>>();
+    var wfcList = new List<Func<Dictionary<string, (int, int)>, Dictionary<string, (int, int)>>>();
     var dstList = new List<string>();
     for (int i = 0; i < m.Groups[2].Captures.Count; ++i)
     {
         var cat = m.Groups[2].Captures[i].Value;
         var op = m.Groups[3].Captures[i].Value;
-        var num = ulong.Parse(m.Groups[4].Captures[i].Value);
+        var num = int.Parse(m.Groups[4].Captures[i].Value);
         dstList.Add(m.Groups[5].Captures[i].Value);
         if (op == "<")
         {
-            wfList.Add((Dictionary<string, (ulong, ulong)> p) => p[cat].Item1 < num ? (p[cat].Item2 < num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (p[cat].Item1, num - 1) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
-            wfcList.Add((Dictionary<string, (ulong, ulong)> p) => p[cat].Item2 >= num ? (p[cat].Item1 >= num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (num, p[cat].Item2) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
+            wfList.Add((Dictionary<string, (int, int)> p) => p[cat].Item1 < num ? (p[cat].Item2 < num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (p[cat].Item1, num - 1) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
+            wfcList.Add((Dictionary<string, (int, int)> p) => p[cat].Item2 >= num ? (p[cat].Item1 >= num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (num, p[cat].Item2) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
         }
         else
         {
-            wfList.Add((Dictionary<string, (ulong, ulong)> p) => p[cat].Item2 > num ? (p[cat].Item1 > num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (num + 1, p[cat].Item2) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
-            wfcList.Add((Dictionary<string, (ulong, ulong)> p) => p[cat].Item1 <= num ? (p[cat].Item2 <= num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (p[cat].Item1, num) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
+            wfList.Add((Dictionary<string, (int, int)> p) => p[cat].Item2 > num ? (p[cat].Item1 > num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (num + 1, p[cat].Item2) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
+            wfcList.Add((Dictionary<string, (int, int)> p) => p[cat].Item1 <= num ? (p[cat].Item2 <= num ? p.ToDictionary(e => e.Key, e => e.Value) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (p[cat].Item1, num) : e.Value)) : p.ToDictionary(e => e.Key, e => e.Key == cat ? (0, 0) : e.Value));
         }
     }
     workflows.Add(src, wfList);
@@ -38,9 +38,9 @@ while ((line = lines[li++]) != "")
     fallbacks.Add(src, fbk);
 }
 
-var accs = new List<Dictionary<string, (ulong, ulong)>>();
-var q = new Queue<(string, Dictionary<string, (ulong, ulong)>)>();
-q.Enqueue(("in", new Dictionary<string, (ulong, ulong)>() { { "x", (1, 4000) }, { "m", (1, 4000) }, { "a", (1, 4000) }, { "s", (1, 4000) } }));
+var accs = new List<Dictionary<string, (int, int)>>();
+var q = new Queue<(string, Dictionary<string, (int, int)>)>();
+q.Enqueue(("in", new Dictionary<string, (int, int)>() { { "x", (1, 4000) }, { "m", (1, 4000) }, { "a", (1, 4000) }, { "s", (1, 4000) } }));
 while (q.Count > 0)
 {
     var p = q.Dequeue();
@@ -64,4 +64,4 @@ while (q.Count > 0)
     }
 }
 
-Console.WriteLine(accs.Select(a => a.Select(a => a.Value.Item2 - a.Value.Item1 + 1).Aggregate((a, c) => a * c)).Aggregate((a, c) => a + c));
+Console.WriteLine(accs.Select(a => a.Select(a => (ulong)(a.Value.Item2 - a.Value.Item1 + 1)).Aggregate((a, c) => a * c)).Aggregate((a, c) => a + c));
