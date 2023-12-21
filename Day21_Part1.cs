e194c145171fdba50179904f595c192c4d9dc0b1
Blair Durkee <blairmadison11@gmail.com>
2023-12-21 02:47:05 -0500

    Update Day21_Part1.cs

--- Day21_Part1.cs
+++ Day21_Part1.cs
@@ -1,67 +1,118 @@
 var grid = File.ReadAllLines("input.txt").Select(l => l.ToArray()).ToArray();
-var fourway = new (int, int)[] { (-1, 0), (0, -1), (0, 1), (1, 0) };
 var plots = new Plot[grid.Length, grid[0].Length];
 Plot start = new Plot(0, 0);
 for (int i = 0; i < grid.Length; ++i)
 {
     for (int j = 0; j < grid[i].Length; ++j)
     {
-        if (grid[i][j] == '.' || grid[i][j] == 'S')
+        if (grid[i][j] == '.')
         {
-            Plot p = plots[i, j];
-            if (p == null)
-            {
-                p = new Plot(i, j);
-                plots[i, j] = p;
-            }
-         
+            plots[i, j] = new Plot(i, j);
+        }
+        else if (grid[i][j] == 'S')
+        {
+            var p = new Plot(i, j);
+            plots[i, j] = p;
+            start = p;
+        }
+    }
+}
+
+var fourway = new (int, int)[] { (-1, 0), (0, -1), (0, 1), (1, 0) };
+for (int i = 0; i < plots.GetLength(0); ++i)
+{
+    for (int j = 0; j < plots.GetLength(1); ++j)
+    {
+        if (plots[i, j] != null)
+        {
+            var p = plots[i, j];
             foreach (var c in fourway.Select(x => (i + x.Item1, j + x.Item2)).Where(x => x.Item1 >= 0 && x.Item1 < grid.Length && x.Item2 >= 0 && x.Item2 < grid[i].Length))
             {
-                if (grid[c.Item1][c.Item2] == '.' || grid[c.Item1][c.Item2] == 'S')
+                if (plots[c.Item1, c.Item2] != null)
                 {
-                    Plot adj = plots[c.Item1, c.Item2];
-                    if (adj == null)
-                    {
-                        adj = new Plot(c.Item1, c.Item2);
-                        plots[c.Item1, c.Item2] = adj;
-                    }
-                    p.AddConnectedPlot(adj);
+                    p.AddConnectedPlot(plots[c.Item1, c.Item2]);
                 }
-            }
-            if (grid[i][j] == 'S')
-            {
-                start = p;
             }
         }
     }
 }
 
 const int LIMIT = 64;
+var rem = LIMIT % 2;
 var endPlots = new HashSet<Plot>();
-var qp = new Queue<Plot>();
-var qs = new Queue<int>();
-qp.Enqueue(start);
-qs.Enqueue(0);
-while (qp.Count > 0)
+if (rem == 0) endPlots.Add(start);
+
+var visited = new HashSet<Plot>();
+var q = new Queue<Path>();
+q.Enqueue(new Path(start));
+while (q.Count > 0)
 {
-    var plot = qp.Dequeue();
-    var steps = qs.Dequeue() + 1;
-    for (int i = 0; i < plot.NextCount; ++i)
+    var path = q.Dequeue();
+    for (int i = 0; i < path.NextCount; ++i)
     {
-        var next = plot.GetAdjacentPlot(i);
-        if (steps == LIMIT)
+        var next = path.GetNext(i);
+        var p2 = new Path(path, next);
+        if (p2.Steps % 2 == rem)
         {
             endPlots.Add(next);
         }
-        else if (!qp.Contains(next))
+        if (p2.Steps != LIMIT && !visited.Contains(next))
         {
-            qp.Enqueue(next);
-            qs.Enqueue(steps);
+            visited.Add(next);
+            q.Enqueue(p2);
         }
     }
 }
 
 Console.WriteLine(endPlots.Count);
+
+using (var sw = new StreamWriter("output.txt"))
+{
+    for (int i = 0; i < grid.Length; ++i)
+    {
+        for (int j = 0; j < grid[i].Length; ++j)
+        {
+            if (plots[i, j] == null)
+            {
+                sw.Write("#");
+            }
+            else if (endPlots.Contains(plots[i, j]))
+            {
+                sw.Write("O");
+            }
+            else
+            {
+                sw.Write(".");
+            }
+        }
+        sw.WriteLine();
+    }
+}
+
+class Path
+{
+    private List<Plot> plots = new List<Plot>();
+    private List<Plot> adj = new List<Plot>();
+
+    public int Steps => plots.Count - 1;
+
+    public Path(Plot start)
+    {
+        plots.Add(start);
+        adj = plots[0].ConnectedPlots.Where(p => !plots.Contains(p)).ToList();
+    }
+
+    public Path(Path p, Plot next)
+    {
+        plots = p.plots.ToList();
+        plots.Add(next);
+        adj = next.ConnectedPlots.Where(p => !plots.Contains(p)).ToList();
+    }
+
+    public int NextCount => adj.Count;
+
+    public Plot GetNext(int index) => adj[index];
+}
 
 class Plot
 {
@@ -71,6 +122,7 @@ class Plot
     public int Row => row;
     public int Col => col;
     public int NextCount => connectedPlots.Count;
+    public List<Plot> ConnectedPlots => connectedPlots;
 
     public Plot(int row, int col)
     {
